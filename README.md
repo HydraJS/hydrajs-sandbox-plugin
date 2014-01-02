a93643571815e2845648d7e09fd0578fda0ab668
tcorral <amischol@gmail.com>
2014-01-02 13:30:57 +0100

    v 1.0.0 First revision

--- README.md
+++ README.md
@@ -1,4 +1,76 @@
-Sandbox
-=======
+# hydrajs-sandbox-plugin
 
-Fallback to the old sandbox system to manage events. 
+Is a plugin to maintain backward compatibility with, Hydra.js, lower version than 2.8.0.
+
+## Update to version 1.0.0
+
+[![Build Status](https://travis-ci.org/HydraJS/hydrajs-sandbox-plugin.png)](https://travis-ci.org/HydraJS/hydrajs-sandbox-plugin)
+
+[Changelog](https://raw.github.com/HydraJS/hydrajs-sandbox-plugin/master/changelog.txt)
+
+## Install
+
+Install with [Bower](http://bower.io)
+
+    bower install hydrajs-sandbox-plugin
+
+Install with [Component](http://component.io)
+
+    component install hydrajs-sandbox-plugin
+
+Install with [NPM](http://npmjs.org)
+
+    npm install hydrajs-sandbox-plugin
+
+### Use in browser
+
+Insert in your html code:
+
+	<script type="text/javascript" src="hydra.js"></script
+	<script type="text/javascript" src="hydrajs-sandbox-plugin.js"></script>
+
+### Use with requirejs
+
+Insert in your code:
+
+    define(['hydrajs-sandbox-plugin'], function () {
+        // code here.
+    });
+
+## How it works
+
+It should be used to manage global events, and this plugin is needed to maintain compatibility with previous version to 2.8.0 of Hydra when the new Bus object has been implemented.
+
+### Common usage
+
+hydrajs-sandbox-plugin extends Hydra.js library adding new methods.
+
+#### new Hydra.action().listen
+
+Using this method your code will start listening events.
+
+    new Hydra.action().listen( ['eventType'], callback, module );
+
+#### new Hydra.action().notify
+
+Using this method your code will trigger the specified event.
+
+    new Hydra.action().notify( { type: 'eventType', data: {} } );
+
+#### new Hydra.action().stopListen
+
+Using this method you code will stop listening events.
+
+    new Hydra.action().stopListen( ['eventType'], module );
+
+## API
+### Hydra
+#### action
+Hydra.action is a constructor that creates the sandbox to manage events and it should be created using *new*.
+
+##### listen - Params [Array - event names, Function - callback to execute when the event is triggered, Object - The context to execute the callback ]
+##### notify - Params [Object - type and data ]
+##### stopListen - Params: [Array - event names, Object - context object where the event is being listened]
+
+# License
+hydrajs-sandbox-plugin is licensed under MIT license. (see LICENSE file)
---
+++ specs/amd-hydrajs-sandbox-pluginSpec.js
@@ -0,0 +1,177 @@
+define( ['hydrajs-sandbox-plugin'], function(){
+
+  describe( 'Hydra Sandbox', function () {
+
+    it( 'should return a function for action property of Hydra', function () {
+      expect( Hydra.action ).toBeDefined();
+      expect( typeof Hydra.action ).toEqual( 'function' );
+    } );
+
+  } );
+
+  describe( 'Hydra Action', function () {
+
+    it( 'should return the Action Class', function () {
+      var oResult;
+
+      oResult = Hydra.action();
+
+      expect( oResult.type ).toEqual( 'Action' );
+    } );
+
+  } );
+
+  describe( 'Hydra Action Listen', function () {
+    var sListener, fpHandler, oModule, oAction;
+
+    beforeEach( function () {
+      sListener = 'test';
+      fpHandler = sinon.stub();
+      oModule = {
+        init: function () {},
+        handleAction: fpHandler,
+        destroy: function () {}
+      };
+      oAction = new Hydra.action();
+      oAction.__restore__();
+    } );
+
+    afterEach( function () {
+      oAction.__restore__();
+    } );
+
+    it( 'should not call fpHandler if notify is launched before set the listeners', function () {
+      oAction.notify( [sListener], { type: sListener } );
+
+      expect( fpHandler.callCount ).toEqual( 0 );
+    } );
+
+    it( 'should call fpHandler if notify is launched after set the listeners', function () {
+      oAction.listen( [sListener], fpHandler, oModule );
+
+      oAction.notify( { type: sListener } );
+
+      expect( fpHandler.callCount ).toEqual( 1 );
+    } );
+
+  } );
+
+  describe( "Hydra action notify", function () {
+    var oAction, fpListen, sListener, sOtherListener, nData, oNotifier, oOtherNotifier, fpHandler, oModule, oErrorHandler, oAction;
+
+    beforeEach( function () {
+      fpListen = sinon.stub();
+      sListener = 'test';
+      sOtherListener = 'test2';
+      nData = 1;
+      oNotifier = {
+        type: sListener,
+        data: nData
+      };
+      oOtherNotifier = {
+        type: sOtherListener,
+        data: nData
+      };
+      fpHandler = function ( oAction ) {
+        if( oAction.type === sListener ) {
+          fpListen();
+        }
+      };
+      oModule = {
+        init: function () {},
+        handleAction: fpHandler,
+        destroy: function () {}
+      };
+      oErrorHandler = Hydra.errorHandler();
+      sinon.stub( oErrorHandler, 'log' );
+      oAction = Hydra.action();
+      oAction.__restore__();
+      oAction.listen( [sListener], fpHandler, oModule );
+    } );
+
+    afterEach( function () {
+      oErrorHandler.log.restore();
+      oAction.__restore__();
+    } );
+
+    it( 'should not call the fpListen callback if the action called is test2', function () {
+      oAction.notify( oOtherNotifier );
+
+      expect( fpListen.callCount ).toEqual( 0 );
+    } );
+
+    it( 'should call the fpListen callback if the action called is test', function () {
+      oAction.notify( oNotifier );
+
+      expect( fpListen.callCount ).toEqual( 1 );
+    } );
+
+    it( 'should not call ErrorHandler.log if debug mode is off', function () {
+      oAction.notify( oNotifier );
+
+      expect( oErrorHandler.log.callCount ).toEqual( 0 );
+    } );
+
+    it( 'should call ErrorHandler.log one time if debug mode is active', function () {
+      var oCall;
+      Hydra.setDebug( true );
+      oAction.notify( oNotifier );
+      oCall = oErrorHandler.log.getCall( 0 );
+
+      expect( oErrorHandler.log.callCount ).toEqual( 1 );
+      expect( oNotifier.type ).toEqual( oCall.args[0] );
+      expect( oNotifier.type ).toEqual( oCall.args[1].type );
+      expect( oCall.args[1].executed.calls ).toEqual( 1 );
+      expect( oModule ).toBe( oCall.args[1].executed.actions[0].module );
+      expect( fpHandler ).toBe( oCall.args[1].executed.actions[0].handler );
+      Hydra.setDebug( false );
+    } );
+
+  } );
+
+  describe( 'Hydra Action Stop Listen', function () {
+    var oAction, fpListen, sListener, nData, oNotifier, fpHandler, oModule, oAction;
+
+    beforeEach( function () {
+      fpListen = sinon.stub();
+      sListener = 'test';
+      nData = 1;
+      oNotifier = {
+        type: sListener,
+        data: nData
+      };
+      fpHandler = function ( oAction ) {
+        if( oAction.type === sListener ) {
+          fpListen();
+        }
+      };
+      oModule = {
+        init: function () {},
+        handleAction: fpHandler,
+        destroy: function () {}
+      };
+      oAction = Hydra.action();
+      oAction.__restore__();
+      oAction.listen( [sListener], fpHandler, oModule );
+    } );
+
+    afterEach( function () {
+      oAction.__restore__();
+    } );
+
+    it( 'should call the fpListen callback if the action called is not stopListen', function () {
+      oAction.notify( oNotifier );
+
+      expect( fpListen.callCount ).toEqual( 1 );
+    } );
+
+    it( 'should not call the fpListen callback if the action called is test if stopListen is called before', function () {
+      oAction.stopListen( [sListener], oModule );
+
+      oAction.notify( oNotifier );
+
+      expect( fpListen.callCount ).toEqual( 0 );
+    } );
+  } );
+
+} );
